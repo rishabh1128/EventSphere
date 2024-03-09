89e9a30ee448ba9a1e866704c0497ff83e1b6c96
Rishabh Jain <rishabh.1514@gmail.com>
2024-03-09 22:10:09 +0530

    Added database models,user server actions,clerk webhook

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -6,4 +6,16 @@ let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing!");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "EventSphere",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
