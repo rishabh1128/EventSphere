e0c715225ca848b6d63c3418f81d1f1a3c4fbc7b
Rishabh Jain <rishabh.1514@gmail.com>
2024-07-02 03:53:57 +0530

    Added event details page, collection and card components, delete event func

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,17 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import Collection from "@/components/shared/Collection";
+import { getAllEvents } from "@/lib/actions/event.actions";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  });
+  // console.log(events);
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -40,6 +49,16 @@ export default function Home() {
         <div className="w-full flex flex-col gap-5 md:flex-row">
           Search CategoryFilter
         </div>
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Check back in later!"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
